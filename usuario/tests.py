570c76429e5fa56d7ee74914032b0a6ff0e0771c
VictorNovais <victorh.novaisr@gmail.com>
2015-02-17 12:19:05 -0200

    Adicionando mommy e criando primeiros testes

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,1 +1,28 @@
-# Create your tests here.
+from django.core.urlresolvers import reverse
+from model_mommy import mommy
+from rest_framework.test import APITestCase
+
+from usuario.models import Usuario
+
+
+class UsuarioTest(APITestCase):
+    def setUp(self):
+        self.usuario = mommy.make(Usuario, cpf='123456789', nome='Trollzinho do LOL')
+        self.token = self.usuario.auth_token.key
+
+    def test_nao_concede_acesso_a_usuario_nao_cadastrado(self):
+        data = {'cpf': 'invalido'}
+        response = self.client.post(reverse('login'), data)
+
+        self.assertEqual(400, response.status_code)
+        self.assertTrue(response.data['non_field_errors'])
+
+    # def test_nao_concede_acesso_a_usuario_em_divida_na_biblioteca(self):
+    #     self.fail()
+
+    def test_concede_acesso_a_usuario_cadastrado(self):
+        data = {'cpf': '123456789'}
+        response = self.client.post(reverse('login'), data)
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.token, response.data['token'])
